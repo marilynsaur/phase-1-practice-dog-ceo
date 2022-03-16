4b375e14cc6ba65e1d8ac9c45fc0711e42c5d8fe
Marilyn Wren <Marilynwren64@yahoo.com>
2022-03-16 16:46:54 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function allDogs() {
     fetch("https://dog.ceo/api/breeds/image/random/4")
         .then(response => response.json())
         .then(data => {
-            console.log(data.message)
+            //console.log(data.message)
 
             data.message.forEach(imgUrl => renderOneDog(imgUrl))
         })
@@ -31,19 +31,20 @@ function renderOneDog(imageUrl) {
 
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 containerBreed = document.querySelector("#dog-breeds");
-
-
+let everyDogs = [];
+filterBreed = document.querySelector("#breed-dropdown");
 function dogBreeds() {
     fetch('https://dog.ceo/api/breeds/list/all')
         .then(response => response.json())
         .then(breeds => {
-            let everyDogs = Object.keys(breeds.message);
-            console.log(everyDogs)
+             everyDogs = Object.keys(breeds.message);
+           // console.log(everyDogs)
             everyDogs.forEach(breed => {
 
                 renderBreedDog(breed)
             })
-            filterDogs(doggy)
+            //filterDogs();
+             console.log(filterBreed)
 
 
         })
@@ -73,14 +74,14 @@ function fontColor(breed) {
     breed.addEventListener('click', function () {
         //li.forEach(breed => breed.add("red"));
         breed.style.color = "red";
-        console.log('everyDogs');
+        //console.log('everyDogs');
     });
 
 }
 
 
 
-filterBreed = document.querySelector("#breed-dropdown");
+
 
 
 
@@ -92,9 +93,33 @@ filterBreed = document.querySelector("#breed-dropdown");
 //.appendChild(dogA);
 
 //};
+dogsA = document.querySelector("#a");
 
-function filterDogs(doggy) {
-    everyDogs.filter(dog => dog.startsWith(doggy));
-    filterBreed.appendChild(doggy);
+
+function filterDogs(event) {
+    //everyDogs;
+    const letter = event.target.value
+    console.log(everyDogs);
+   const siftDogs = everyDogs.filter(dog => dog.startsWith(letter));
+   everyDogs.forEach(breed => {
+    containerBreed.innerHTML = `  `
+    renderBreedDog(siftDogs)
+})
+   
+    //
+   //for (let i=0;i<everyDogs.length;i++){
+   //// everyDogs[i].startsWith("a")
+   //const doggyA = filterBreed.addEventListener('input', function (event) {
+       // console.log(event.target.value);
+       //console.log(doggyA)
+       //if (event.target.id !== 'a') return filterDogA;
+   // });
+   //doggyA.appendChild(doggy);
+//}
 }
-filterBreed.addEventListener('click',filterDogs(a));
+filterBreed.addEventListener('change',filterDogs); 
+
+
+   
+
+//filterA.addEventListener('change',filterA);
